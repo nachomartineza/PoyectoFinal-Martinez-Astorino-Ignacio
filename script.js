9b5e4b81f34c5b6997a60b0b51583ab7ca135316
nachomartineza <nachomartinezps4@gmail.com>
2023-03-22 13:46:05 -0300

    Correcciones hechas

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
 let nombre = prompt("Usted ingresó a la Casa de cambio! Ingrese su nombre");
-console.log (nombre)
-for (let i=0; i<2; i++){
-    if (nombre !== ""){
-        console.log("introdujo nombre")
-      break
-        } else {
-          nombre = prompt("Usted ingresó a la Casa de cambio! Ingrese su nombre");
-        }
-    }
+console.log(nombre);
 
+while (nombre === "") {
+  nombre = prompt("Usted ingresó a la Casa de cambio! Ingrese su nombre");
+}
+
+console.log("introdujo nombre");
 
 function convertir() {
-    let valore = parseInt(document.getElementById("valor").value);
-    let resultado = 0;
-    let dolar = 379;
-    let euro = 424;
-    const moneda = {nombre:"dolar",valor:"379", nombre:"euro",valor:"424"};
-    if (document.getElementById("uno").checked) {
-        resultado = valore/dolar;
-        alert ("El cambio de pesos a dolares es: $" + resultado.toFixed(2));
-    }
+  let valore = parseInt(document.getElementById("valor").value);
+  let resultado = 0;
+  const monedas = [
+    { nombre: "dolar", valor: 379 },
+    { nombre: "euro", valor: 424 }
+  ];
 
-    else if (document.getElementById("dos").checked) {
-        resultado = valore/euro;
-        alert ("El cambio de pesos a euros es: €" + resultado.toFixed(2));
-    }
-    else {
-        alert ("Tenés que completar todos los campos")
-    }
+  if (document.getElementById("uno").checked) {
+    resultado = valore / monedas[0].valor;
+    alert("El cambio de pesos a dolares es: $" + resultado.toFixed(2));
+  } else if (document.getElementById("dos").checked) {
+    resultado = valore / monedas[1].valor;
+    alert("El cambio de pesos a euros es: €" + resultado.toFixed(2));
+  } else {
+    alert("Tenés que completar todos los campos");
+  }
 }
