b64f3c82ba10510141b7202d68dc1c061923bf25
nachomartineza <nachomartinezps4@gmail.com>
2023-03-29 11:44:33 -0300

    Casi terminado

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
-let nombre = prompt("Usted ingresó a la Casa de cambio! Ingrese su nombre");
-console.log(nombre);
+let nombre = localStorage.getItem("nombreUsuario");
 
-while (nombre === "") {
-  nombre = prompt("Usted ingresó a la Casa de cambio! Ingrese su nombre");
+if (!nombre) {
+    nombre = prompt("Usted ingresó a la Casa de cambio! Ingrese su nombre");
+
+    while (nombre === "") {
+        nombre = prompt("Usted ingresó a la Casa de cambio! Ingrese su nombre");
+    }
+
+    localStorage.setItem("nombreUsuario", nombre);
 }
 
-console.log("introdujo nombre");
+console.log(nombre);
 
 function convertir() {
-  let valore = parseInt(document.getElementById("valor").value);
-  let resultado = 0;
-  const monedas = [
-    { nombre: "dolar", valor: 393 },
-    { nombre: "euro", valor: 441 }
-  ];
+    let valore = parseInt(document.getElementById("valor").value);
+    let resultado = 0;
+    let monedaNombre = "";
+    let monedaValor = 0;
 
-  if (document.getElementById("uno").checked) {
-    resultado = valore / monedas[0].valor;
-    alert("El cambio de pesos a dolares es: $" + resultado.toFixed(2));
-  } else if (document.getElementById("dos").checked) {
-    resultado = valore / monedas[1].valor;
-    alert("El cambio de pesos a euros es: €" + resultado.toFixed(2));
-  } else {
-    alert("Tenés que completar todos los campos");
-  }
+    const monedas = [
+        { nombre: "dólar", valor: 393 },
+        { nombre: "euro", valor: 441 },
+    ];
+
+    if (document.getElementById("uno").checked) {
+        monedaNombre = monedas[0].nombre;
+        monedaValor = monedas[0].valor;
+        resultado = valore / monedas[0].valor;
+        localStorage.setItem("dolaresConvertidos", resultado.toFixed(2));
+    } else if (document.getElementById("dos").checked) {
+        monedaNombre = monedas[1].nombre;
+        monedaValor = monedas[1].valor;
+        resultado = valore / monedas[1].valor;
+        localStorage.setItem("eurosConvertidos", resultado.toFixed(2));
+    } else {
+        alert("Tenés que seleccionar una moneda");
+    }
+
+    if (monedaNombre && monedaValor) {
+        alert(`El cambio de pesos a ${monedaNombre} es: ${monedaValor} ${monedaNombre.toUpperCase()} = $${resultado.toFixed(2)}`);
+    }
 }
