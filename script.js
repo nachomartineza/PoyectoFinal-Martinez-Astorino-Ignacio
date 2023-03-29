41731cf42ee429a62a80c6bb90f459a2e8537021
nachomartineza <nachomartinezps4@gmail.com>
2023-03-29 11:55:17 -0300

    ahora sim

--- script.js
+++ script.js
@@ -1,43 +1,58 @@
 let nombre = localStorage.getItem("nombreUsuario");
 
 if (!nombre) {
+  nombre = prompt("Usted ingresó a la Casa de cambio! Ingrese su nombre");
+
+  while (nombre === "") {
     nombre = prompt("Usted ingresó a la Casa de cambio! Ingrese su nombre");
+  }
 
-    while (nombre === "") {
-        nombre = prompt("Usted ingresó a la Casa de cambio! Ingrese su nombre");
-    }
-
-    localStorage.setItem("nombreUsuario", nombre);
+  localStorage.setItem("nombreUsuario", nombre);
 }
 
 console.log(nombre);
 
+const formulario = document.getElementById("formulario");
+const cotizador = document.getElementById("cotizador");
+
+formulario.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  convertir();
+});
+
+cotizador.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  convertir();
+});
+
 function convertir() {
-    let valore = parseInt(document.getElementById("valor").value);
-    let resultado = 0;
-    let monedaNombre = "";
-    let monedaValor = 0;
+  let valore = parseInt(document.getElementById("valor").value);
+  let resultado = 0;
+  let monedaNombre = "";
+  let monedaValor = 0;
 
-    const monedas = [
-        { nombre: "dólar", valor: 393 },
-        { nombre: "euro", valor: 441 },
-    ];
+  const monedas = [
+    { nombre: "dólar", valor: 393 },
+    { nombre: "euro", valor: 441 },
+  ];
 
-    if (document.getElementById("uno").checked) {
-        monedaNombre = monedas[0].nombre;
-        monedaValor = monedas[0].valor;
-        resultado = valore / monedas[0].valor;
-        localStorage.setItem("dolaresConvertidos", resultado.toFixed(2));
-    } else if (document.getElementById("dos").checked) {
-        monedaNombre = monedas[1].nombre;
-        monedaValor = monedas[1].valor;
-        resultado = valore / monedas[1].valor;
-        localStorage.setItem("eurosConvertidos", resultado.toFixed(2));
-    } else {
-        alert("Tenés que seleccionar una moneda");
-    }
+  if (document.getElementById("uno").checked) {
+    monedaNombre = monedas[0].nombre;
+    monedaValor = monedas[0].valor;
+    resultado = valore / monedas[0].valor;
+    localStorage.setItem("dolaresConvertidos", resultado.toFixed(2));
+  } else if (document.getElementById("dos").checked) {
+    monedaNombre = monedas[1].nombre;
+    monedaValor = monedas[1].valor;
+    resultado = valore / monedas[1].valor;
+    localStorage.setItem("eurosConvertidos", resultado.toFixed(2));
+  } else {
+    alert("Tenés que seleccionar una moneda");
+  }
 
-    if (monedaNombre && monedaValor) {
-        alert(`El cambio de pesos a ${monedaNombre} es: ${monedaValor} ${monedaNombre.toUpperCase()} = $${resultado.toFixed(2)}`);
-    }
+  if (monedaNombre && monedaValor) {
+    alert(`El cambio de pesos a ${monedaNombre} es: ${monedaValor} ${monedaNombre.toUpperCase()} = $${resultado.toFixed(2)}`);
+  }
 }
